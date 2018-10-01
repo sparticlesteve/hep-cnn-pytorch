f36f9fff66fb3491035cbb848557b490737e961d
Steve Farrell <sparticlesteve@gmail.com>
2018-10-01 16:28:35 -0700

    switch to my private build of pytorch master

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 export OMP_NUM_THREADS=32
 export KMP_AFFINITY="granularity=fine,compact,1,0"
 export KMP_BLOCKTIME=1
-module load pytorch-mpi/v0.4.1
+
+# Trying my master build
+export MPICH_MAX_THREAD_SAFETY=multiple
+conda activate /global/cscratch1/sd/sfarrell/conda/pytorch-mpi/b0ad810
